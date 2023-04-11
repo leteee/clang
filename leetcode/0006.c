5b23f5a83a2f756882c0678d939df911bd70e9c2
lete <leiteng712@qq.com>
2023-04-11 08:41:56 +0800

    feat: 力扣第六题

--- leetcode/0006.c
+++ leetcode/0006.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
 
 //将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
@@ -8,17 +9,38 @@
 //Y   I   R
 //之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
 
-char *convert(char *s, int len, int numRows) {
-    if (numRows < 2 || numRows >= len) {
+char *convert(char *s, int numRows) {
+    int n = strlen(s), r = numRows;
+    if (r == 1 || r >= n) {
         return s;
     }
-    // todo
+    char **mat = (char **) malloc(sizeof(char *) * r);
+    int *columSize = (int *) malloc(sizeof(int) * r);
+    for (int i = 0; i < r; ++i) {
+        int size = sizeof(char) * (n + 1);
+        mat[i] = (char *) malloc(size);
+        memset(mat[i], 0, size);
+    }
+    for (int i = 0, x = 0, t = r * 2 - 2; i < n; ++i) {
+        mat[x][columSize[x]++] = s[i];
+        i % t < r - 1 ? ++x : --x;
+    }
+    int pos = 0;
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < columSize[i]; ++j) {
+            s[pos++] = mat[i][j];
+        }
+        free(mat[i]);
+    }
+    free(columSize);
+    free(mat);
+    return s;
 }
 
 
 void test() {
     char s[] = "PAYPALISHIRING";
-    char *ret = convert(s, sizeof(s) / sizeof(char), 3);
+    char *ret = convert(s, 3);
     printf("%s", ret);
 }
 
